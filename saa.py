3ee36c735edf89c7f37c9fcb314abaeedbdf4b3b
edwsqa <mazkuuuu@gmail.com>
2020-10-23 13:10:05 +0300

    Add files via upload

---
+++ saa.py
@@ -0,0 +1,53 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+import csv
+
+url = 'https://www.foreca.fi/Finland/Kajaani/10vrk'
+
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+uClient = urlopen(req)
+page_html = uClient.read()
+uClient.close()
+
+
+page_soup = soup(page_html, "html.parser")
+
+containers = page_soup.findAll("div",{"class":"day"})
+
+filename = "Weather.csv"
+f = open(filename, "w")
+
+headers = "Päivämäärä, Sää, Ylin, Alin, Tuuli\n"
+f.write(headers)
+
+
+for container in containers:
+    
+    Day_container = container.findAll("h5")
+    Day = Day_container[0].text
+
+    weather = container.a["title"]
+
+    Max_container = container.findAll("span", {"class":"warm"})
+    Max = Max_container[0].text
+
+    Max_container = container.findAll("span", {"class":"cold"})
+    Max = Max_container[0].text
+
+    Min_container = container.findAll("span", {"class":"cold"})
+    Min = Min_container[0].text
+
+
+    Wind_container = container.findAll("div", {"class":"ws"})
+    Wind_speed = Wind_container[0].text
+
+    print("Päivämäärä: " + Day + "\n")
+    print("Sää: " + weather + "\n")
+    print("Ylin: " + Max + "\n")
+    print("Alin: " + Min + "\n")
+    print("Tuuli: " + Wind_speed + "\n")
+
+    f.write(Day + "," + weather + "," + Max + "," + Min + "," + Wind_speed + "\n")
+
+
+f.close()
