0e6af7192fc228b6d460fcf1cf016e7004766f78
edwsqa <mazkuuu@gmail.com>
2020-11-12 09:00:02 +0000

    Automatic commit and push

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-#<?php
-#header("Location: http://files-scraper.rahtiapp.fi/files ");
-#exit();
-#?>
-
 <?php
-echo "<html><body><table>\n\n";
-$f = fopen("files/jimms.csv", "r");
-while (($line = fgetcsv($f)) !== false) {
-        echo "<tr>";
-        foreach ($line as $cell) {
-                echo "<td>" . htmlspecialchars($cell) . "</td>";
-        }
-        echo "</tr>\n";
-}
-fclose($f);
-echo "\n</table></body></html>";
+header("Location: http://files-scraper.rahtiapp.fi/files ");
+exit();
+?>
+
+#<?php
+#echo "<html><body><table>\n\n";
+#$f = fopen("files/jimms.csv", "r");
+#while (($line = fgetcsv($f)) !== false) {
+#        echo "<tr>";
+#        foreach ($line as $cell) {
+#                echo "<td>" . htmlspecialchars($cell) . "</td>";
+#        }
+#        echo "</tr>\n";
+#}
+#fclose($f);
+#echo "\n</table></body></html>";
